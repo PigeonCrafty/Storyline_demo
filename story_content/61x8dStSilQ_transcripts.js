038f8ad9613adaa7d803ca5e8d871e46ef87e877
PigeonCraft <36757441+PigeonCrafty@users.noreply.github.com>
2023-12-12 14:27:46 +0800

    upload

---
+++ story_content/61x8dStSilQ_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":40,"text":"作为新任线组长"},{"start":1319,"text":"为什么要学习角色认知"},{"start":3952,"text":"因为从岗位能手成为管理者"},{"start":7015,"text":"是职业生涯的关键转型"},{"start":10310,"text":"工作职责、工作方法都发生了巨大变化"},{"start":14780,"text":"只有先明确新角色的定位"},{"start":17078,"text":"才能更有针对性和系统性地学习"}]}]};
+    window.globalLoadJsAsset('story_content/61x8dStSilQ_transcripts.js', JSON.stringify(data));
+})();
